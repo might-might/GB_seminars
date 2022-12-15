e991d5a211c80b36662e658ce5379c4115934d0f
might_might <bcmactive@gmail.com>
2022-12-15 11:58:03 +0300

    5th done

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,30 +1,9 @@
 ﻿Console.WriteLine("Введите число: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-if (numberA == 1)
-{
-    Console.WriteLine($"{numberA} - это понедельник");
-}
-else if (numberA == 2)
-{
-    Console.WriteLine($"{numberA} - это вторник");
-}
-else if (numberA == 3)
-{
-    Console.WriteLine($"{numberA} - это среда");
-}
-else if (numberA == 4)
-{
-    Console.WriteLine($"{numberA} - это четверг");
-}
-else if (numberA == 5)
-{
-    Console.WriteLine($"{numberA} - это пятница");
-}
-else if (numberA == 6)
-{
-    Console.WriteLine($"{numberA} - это суббота");
-}
-else if (numberA == 7)
-{
-    Console.WriteLine($"{numberA} - это воскресенье");
-}
+string numberA = Console.ReadLine();
+if (numberA == "1") Console.WriteLine($"{numberA} - это понедельник");
+else if (numberA == "2") Console.WriteLine($"{numberA} - это вторник");
+else if (numberA == "3") Console.WriteLine($"{numberA} - это среда");
+else if (numberA == "4") Console.WriteLine($"{numberA} - это четверг");
+else if (numberA == "5") Console.WriteLine($"{numberA} - это пятница");
+else if (numberA == "6") Console.WriteLine($"{numberA} - это суббота");
+else if (numberA == "7") Console.WriteLine($"{numberA} - это воскресенье");
