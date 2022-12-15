abeb56ab99cf7bcd3eb61e5b75a9cd52573ad041
might_might <bcmactive@gmail.com>
2022-12-15 12:44:48 +0300

    task07 done

---
+++ Task07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трезначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA >= 100 && numberA <= 999)
+{
+    int result = numberA % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Вы ввели нетрехзначное число");
+}
