ef978270c625c41f794b76838e349d5a546bd57d
might_might <bcmactive@gmail.com>
2022-12-15 12:33:58 +0300

    task05 done

---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 5: Напишите программу, которая на вход принмает одно число N, а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите натуральное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int counter = -numberN;
+while (counter <= numberN)
+{
+    if (counter < numberN)
+    {
+        Console.Write(counter + ", ");
+    }
+    else if (counter == numberN)
+    {
+        Console.WriteLine(numberN);
+    }
+    counter += 1;
+}
