87fdef6b5a1182ec3d1cfc0ded0e5a910752627b
might-might <bcmactive@gmail.com>
2022-12-21 20:07:25 +0300

    12th done

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,17 +1,18 @@
-﻿void AdivB(int numA, int numB)
+﻿Console.WriteLine("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+void AdivB(int numA, int numB)
 {
-    Console.WriteLine("Введите первое число:");
-    int numberA = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите второе число:");
-    int numberB = Convert.ToInt32(Console.ReadLine());
     int result = numberA % numberB;
     if (result != 0)
         {
-            Console.WriteLine($"не кратно, остаток {numberA % numberB}");
+            Console.WriteLine($"Число {numberA} не кратно числу {numberB}, остаток {numberA % numberB}");
         }
     else
     {
         Console.WriteLine($"Число {numberA} кратно числу {numberB}");
     }
 }
-AdivB(20, 10);
+AdivB(numberA, numberB);
