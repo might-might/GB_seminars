b6627263be46493949cd0fe6c6322b9e07f088bb
might-might <bcmactive@gmail.com>
2022-12-21 18:41:26 +0300

    10th added

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число => {number}");
+
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+Console.WriteLine(firstDigit);
+Console.WriteLine(thirdDigit);
+Console.WriteLine($"{firstDigit}{thirdDigit}");
