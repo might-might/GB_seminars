6b92ab638fdb384fd4d3ec6122842a033528d5e9
might-might <bcmactive@gmail.com>
2022-12-22 00:05:10 +0300

    13th done!

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,12 +3,24 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
-
-Console.Write("Введите натуральное число: ")
+Console.Write("Введите натуральное число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-void ThirdDigit(num)
+void ThirdDigit(int num)
 {
-    result = numberA / 10
-    if 
+    int counter = 1;
+    int result = num / 10;
+    while (counter < result)
+    {
+        if (result < 10)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+        else if (10 * counter <= result & result < 100 * counter)
+        {
+            Console.WriteLine($"Третья цифра - {num / counter % 10}");
+        }
+        counter *= 10;
+    }
 } 
+ThirdDigit(numberA);
