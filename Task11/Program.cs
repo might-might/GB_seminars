9bc70de5f3cb457c6d7a84f9b1e870b9877d29c9
might-might <bcmactive@gmail.com>
2022-12-21 19:15:59 +0300

    11th added

---
+++ Task11/Program.cs
@@ -0,0 +1,17 @@
+﻿void AdivB(int numA, int numB)
+{
+    Console.WriteLine("Введите первое число:");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число:");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    int result = numberA % numberB;
+    if (result != 0)
+        {
+            Console.WriteLine($"не кратно, остаток {numberA % numberB}");
+        }
+    else
+    {
+        Console.WriteLine($"Число {numberA} кратно числу {numberB}");
+    }
+}
+AdivB(20, 10);
