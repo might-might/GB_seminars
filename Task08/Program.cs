1bd47c9ca233fbd861f2ae24c31c5b86514fe4dd
might_might <bcmactive@gmail.com>
2022-12-22 19:48:44 +0300

    9th added

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -5,26 +5,15 @@
 Console.WriteLine("Введите натуральное число: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 int i = 2;
-if (numberN <= 0)
+while (i <= numberN)
 {
-    Console.WriteLine("Вы ввели неверное число.");
+if (i < numberN - 1)
+{
+Console.Write(i + ", ");
 }
 else
 {
-    while (i <= numberN)
-    {
-        if (i + 1 == numberN)
-        {
-            Console.WriteLine(i);
-        }
-        else if (i < numberN)
-        {
-            Console.Write(i + ", ");
-        }
-        else if (i == numberN)
-        {
-            Console.WriteLine(i);
-        }
-        i += 2;
-    }
+Console.WriteLine(i);
+}
+i += 2;
 }
---
+++ Task09/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из диапазона 10 - 99 => {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.Write("Наибольшая цифра числа - ");
+Console.WriteLine(result);
