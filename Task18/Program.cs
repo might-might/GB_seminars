266544f3717b75a3a1cd6a1e481b7af613fca33d
might_might <bcmactive@gmail.com>
2022-12-22 22:05:17 +0300

    18th added

---
+++ Task18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер четверти:");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string range = Range(quarter);
+Console.WriteLine(range);
+
+string Range(int num)
+{
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x < 0, y > 0";
+    if (num == 3) return "x < 0, y < 0";
+    if (num == 4) return "x > 0, y < 0";
+    return "Введен некорректный номер четверти";
+}
