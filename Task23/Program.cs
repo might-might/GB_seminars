dd5786d675d327e03097f621cb4e612a3445596f
might_might <bcmactive@gmail.com>
2022-12-23 01:27:41 +0300

    23th done

---
+++ Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 -> 
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+// Напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SqrtTable(int num)
+{
+    if (num <= 0) Console.WriteLine("Вы ввели некорректное число");
+    else
+    {
+        int counter = 1;
+        while (counter <= num)
+        {
+            Console.WriteLine($"{counter} | {Math.Pow(counter, 3)}");
+            counter++;
+        }
+    }
+}
+
+SqrtTable(number);
