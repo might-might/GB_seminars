27ca0751dc595c1d2a059aa83ad4ce24523341e6
might_might <bcmactive@gmail.com>
2023-01-18 19:51:08 +0300

    27th done

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите натуральное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+while (number1 < 0)
+{
+    Console.WriteLine("Вы ввели ненатуральное число. Попробуйте снова.");
+    Console.WriteLine("Введите натуральное число");
+    number1 = int.Parse(Console.ReadLine());
+}
+
+int sum = 0;
+int SumNumbers(int num)
+{
+    while (num > 0)
+    {
+        sum = num % 10 + sum;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine(SumNumbers(number1));
