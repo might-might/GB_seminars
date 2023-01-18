e19393b35f5f37e45996d63210e650755deb8298
might_might <bcmactive@gmail.com>
2023-01-18 18:49:27 +0300

    25th done

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -8,7 +8,7 @@
 Console.WriteLine("Введите пятизначное число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number / 10000 < 1 || number / 10000 >= 10) Console.WriteLine("Вы ввели некорректное число.");
+if (number / 10000 < 1) Console.WriteLine("Вы ввели некорректное число.");
 else
 {
     if (Palindrome(number)) Console.WriteLine($"{number} -> да");
@@ -17,10 +17,43 @@ else
 
 bool Palindrome(int num)
 {
-    int digit1 = num / 10000;
-    int digit2 = num / 1000 % 10;
-    int digit4 = num / 10 % 10;
-    int digit5 = num % 10;
-    if (digit5 == digit1 && digit4 == digit2) return true;
+    // int digit1 = num / 10000;
+    // int digit2 = num / 1000 % 10;
+    // int digit3 = num / 100 % 10;
+    // int digit4 = num / 10 % 10;
+    // int digit5 = num % 10;
+    // if (digit5 == digit1 && digit4 == digit2) return true;
+    // else return false;
+    
+    // int digit6 = num / 100000;
+    // int digit5 = num / 10000;
+    // int digit4 = num / 1000 % 10;
+    // int digit3 = num / 100 % 10;
+    // int digit2 = num / 10 % 10;
+    // int digit1 = num % 10;
+    // if (digit1 == digit6 && digit2 == digit5 && digit3 == digit4) return true;
+    // else return false;
+    
+    // int digit7 = num / 1000000;
+    // int digit6 = num / 100000;
+    // int digit5 = num / 10000;
+    // int digit4 = num / 1000 % 10;
+    // int digit3 = num / 100 % 10;
+    // int digit2 = num / 10 % 10;
+    // int digit1 = num % 10;
+    // if (digit1 == digit7 && digit2 == digit6 && digit3 == digit5) return true;
+    // else return false;
+    
+    int digit8 = num / 10000000;
+    int digit7 = num / 1000000 % 10;
+    int digit6 = num / 100000 % 10;
+    int digit5 = num / 10000 % 10;
+    int digit4 = num / 1000 % 10;
+    int digit3 = num / 100 % 10;
+    int digit2 = num / 10 % 10;
+    int digit1 = num % 10;
+    if (digit1 == digit8 && digit2 == digit7 && digit3 == digit6 && digit4 == digit5) return true;
     else return false;
+    //  Console.WriteLine(digit1);
 }
+// Palindrome(number);
---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число");
+int number2 = int.Parse(Console.ReadLine());
+while (number2 < 0)
+{
+    Console.WriteLine("Вы ввели ненатуральное число. Попробуйте снова.");
+    Console.WriteLine("Введите натуральное число");
+    number2 = int.Parse(Console.ReadLine());
+}
+
+int numberQuad(int num1, int num2)
+{
+    int result = num1;
+    for (int i = 2; i <= num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+Console.WriteLine(numberQuad(number1, number2));
