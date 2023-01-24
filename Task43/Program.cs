75ce45fa5a69d6b4a07cc3940903e380a61f5c0a
might_might <bcmactive@gmail.com>
2023-01-25 02:51:34 +0300

    43th done but need some marks for exceptions

---
+++ Task43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Write("Вам нужно задать коэффициенты для первой прямой. Введите целое k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Теперь нужно задать коэффициенты для второй прямой. Введите целое k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+double[] WhatIsThePoint(int coefK1, int coefB1, int coefK2, int coefB2)
+{
+    double diff = coefB2 - coefB1;
+    double diff_2 = coefK1 - coefK2;
+    double x = diff / diff_2;
+    double y = coefK1 * x + coefB1;
+    double[] array = new double[2];
+    array[0] = x;
+    array[1] = y;
+    return array;
+}
+
+
+void PrintResult(double[] arr)
+{
+    Console.Write($"({arr[0]}; {arr[1]})");
+}
+
+double diff = b2 - b1;
+double diff_2 = k1 - k2;
+double x = diff / diff_2;
+double y = k1 * x + b1;
+Console.WriteLine(x);
+Console.WriteLine(y);
+WhatIsThePoint(k1, b1, k2, b2);
+PrintResult(WhatIsThePoint(k1, b1, k2, b2));
